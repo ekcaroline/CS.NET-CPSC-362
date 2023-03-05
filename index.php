cf076910e5ff357e1ba2e7f82bbfe417cd3f6ddd
David <124234643+Pzychopomp@users.noreply.github.com>
2023-03-04 20:49:10 -0800

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>CS.NET</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body> 
+    <center>
+    <brand_title> CS.NET &#x26FE </brand_title> 
+    </center>
+    <center>
+       <a class="button_style1" href="logincsnet.php">Login</a>
+      <a class="button_style1" href="profilecreation.php">Create an Account</a>
+    </center>
+    <whttxt>
+      Copyright &copy;
+    </whttxt>
+  </body>
+</html>
